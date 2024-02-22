7125c80508a240b8778b9c33336dec9a122e363b
Chanbok Wi <cksqhr4961@gmail.com>
2024-02-22 20:37:06 +0900

    first commit

---
+++ src/main/java/kr/dogglezz/querycounter/QueryCounterApplication.java
@@ -0,0 +1,13 @@
+package kr.dogglezz.querycounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/dogglezz/querycounter/QueryCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.dogglezz.querycounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
